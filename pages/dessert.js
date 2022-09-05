babe1ec703f78ef0073cdb784e2cb037eaa8e879
Merle Makoben <merle.makoben@neuefische.de>
2022-09-05 11:43:36 +0200

    Add image component with remote image from another domain

--- pages/dessert.js
+++ pages/dessert.js
@@ -1,7 +1,10 @@
+import Image from "next/image"
+
 export default function Dessert(){
 return (
 <>
 <h1>Headline</h1>
 <p>Short paragraph</p>
+<Image src="https://unsplash.com/photos/surQ2mkZNxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mzd8fGRlc3NlcnR8ZW58MHwwfHx8MTY1OTMzNzk1NQ&force=true&w=2400" width={2400} height={1599} layout="responsive"/>
 </>
 )}
