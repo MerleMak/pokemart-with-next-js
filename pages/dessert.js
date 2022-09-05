af0ce88a3beed2fe107ec0be8df531b5071a8825
Merle Makoben <merle.makoben@neuefische.de>
2022-09-05 11:12:19 +0200

    Clean up of project folder

---
+++ pages/dessert.js
@@ -0,0 +1,7 @@
+export default function Dessert(){
+return (
+<>
+<h1>Headline</h1>
+<p>Short paragraph</p>
+</>
+)}
---
+++ pages/veggie.js
@@ -0,0 +1,7 @@
+export default function Veggie(){
+    return (
+    <>
+    <h1>Headline Veggie</h1>
+    <p>Short Veggie paragraph</p>
+    </>
+    )}
