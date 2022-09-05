d6b4955f4bfccef96e5ce3248f388e3d0555f087
Merle Makoben <merle.makoben@neuefische.de>
2022-09-05 11:36:20 +0200

    Add image component

--- pages/veggie.js
+++ pages/veggie.js
@@ -1,7 +1,10 @@
+import Image from "next/image"
+
 export default function Veggie(){
     return (
     <>
     <h1>Headline Veggie</h1>
     <p>Short Veggie paragraph</p>
+    <Image src="/pizza.jpg" width={2400} height={1597} layout="responsive"/>
     </>
     )}
